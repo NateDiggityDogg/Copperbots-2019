1a3c5e203449787d64f0b9aef35d6bfa468833d5
Dereck Wonnacott <dereck@gmail.com>
2018-03-07 09:51:29 -0500

    Auton Fixes
    * Added AutoTurn() - The way the autodrive function was written, would
    have caused the robot to try and hold an encoder count of zero while
    turning, which would have caused a sort of pivot turn / arc turn rather
    than a dead-spin. I don't think this is desirable, so I split the
    function in two.
    * Added absMax() - to limit drive motor speed, reduce typo potential,
    increase code reuse.
    * We never reset the gyro, so the auto turn feature would not have
    worked as intended. That's ok, because if we avoid resetting the gyro we
    get better overall accuracy, but the code needs to be updated to work
    correctly.
    * Apparently I deleted the limitBreak control last night... (fixed)

--- 2018---The REAL First Power Up/src/org/usfirst/frc/team2586/robot/Robot.java
+++ 2018---The REAL First Power Up/src/org/usfirst/frc/team2586/robot/Robot.java
@@ -88,7 +88,8 @@ public class Robot extends TimedRobot {
 	Timer autoTimer;
 
 	// autoDrive state vars
-	private double dPrev = 0.0;
+	double dPrev = 0.0;
+	double dHeading = 0.0;
 
 	/**
 	 * -------------------------------------------------------------------------------------------------------------------------------
@@ -212,6 +213,13 @@ public class Robot extends TimedRobot {
 		// Lift Control
 		double liftCommand = operatorController.getRawAxis(1) * -1;
 
+		// Override all soft limits
+		if (operatorController.getBackButton())
+			limitBreak = true;
+
+		if (operatorController.getStartButton())
+			limitBreak = false;
+
 		// Send speed command to the lift
 		if (limitBreak) {
 			lift.set(liftCommand);
@@ -270,6 +278,7 @@ public class Robot extends TimedRobot {
 	public void autonomousInit() {
 		// Reset Gyro heading
 		gyro.reset();
+		dHeading = 0.0;
 
 		// Restart the auto sequence
 		autoStep = 0;
@@ -317,7 +326,7 @@ public class Robot extends TimedRobot {
 
 		case 2:
 			// Stop!
-			mainDrive.arcadeDrive(0.0, 0.0);
+			mainDrive.stopMotor();
 
 			// Hammer time... :)
 			break;
@@ -336,39 +345,44 @@ public class Robot extends TimedRobot {
 	private void autoProgSwitchCenter() {
 
 		// Pick a direction based on FMS data
-		double rot = 90;
+		double rot = 45;
 		if (gameData.startsWith("R"))
-			rot = -90;
+			rot = -rot;
 
 		// Switch case for AUTONOMOUS SWITCH
 		switch (autoStep) {
 
 		case 1:
-			if (autoDrive(24.0, 0.0))
+			if (autoDrive(24.0))
 				autoNextStep();
 			break;
 
 		case 2:
-			if (autoDrive(0.0, rot))
+			if (autoTurn(rot))
 				autoNextStep();
 			break;
 
 		case 3:
-			if (autoDrive(48.0, -rot))
+			if (autoDrive(48.0))
 				autoNextStep();
 			break;
 
 		case 4:
-			if (autoDrive(36.0, 0.0))
+			if (autoTurn(0.0))
 				autoNextStep();
 			break;
 
 		case 5:
+			if (autoDrive(36.0))
+				autoNextStep();
+			break;
+
+		case 6:
 			// Drop it like it's hot...
 			clamp.set(DoubleSolenoid.Value.kReverse); // Open
 
 			// Stop!
-			mainDrive.arcadeDrive(0.0, 0.0);
+			mainDrive.stopMotor();
 
 			break;
 		}
@@ -394,7 +408,10 @@ public class Robot extends TimedRobot {
 	 * 
 	 * Must reset encoders and autoTimer between steps.
 	 */
-	private boolean autoDrive(double distance, double angle) {
+	private boolean autoDrive(double distance) {
+
+		// Max drive speed
+		// TODO: Increase / remove after validation.
 		double maxSpeed = 0.4;
 
 		//
@@ -415,40 +432,31 @@ public class Robot extends TimedRobot {
 
 		// Proportional control to get started
 		// TODO: add integral term later perhaps
-		/** Where is kP derived from? */
 		double kP = maxSpeed / 36.0; // Start to slow down at 36 inches from target
-		double e_lin = (distance - d);
+		double e_lin = distance - d;
 		double lin = e_lin * kP;
 
 		// Ramp up to speed to reduce wheel slippage
 		// TODO: We could probably use .getRate() and control the actual acceleration...
-		/** Is this max acceleration speed? */
 		double max_ramp_up = 0.075;
 		if (lin > dPrev + max_ramp_up)
 			lin = dPrev + max_ramp_up;
 		dPrev = lin;
 
-		// Limit max speed while testing...
-		// TODO: Increase / remove after validation.
-		if (lin > 0)
-			lin = Math.min(lin, maxSpeed);
-		else
-			lin = Math.max(lin, -maxSpeed);
+		// Limit max speed
+		lin = absMax(lin, maxSpeed);
 
 		//
 		// Rotation
 		//
+
 		/** Where is kP_rot derived from? & why 0.5? */
 		double kP_rot = maxSpeed / 45.0; // start slowing down at 45 deg.
-		double a = gyro.getYaw();
-		double e_rot = angle - a;
+		double e_rot = dHeading - gyro.getYaw();
 		double rot = e_rot * kP_rot;
 
 		// Max rotation speed
-		if (rot > 0)
-			rot = Math.min(lin, maxSpeed);
-		else
-			rot = Math.max(lin, -maxSpeed);
+		rot = absMax(rot, maxSpeed);
 
 		// Nothing left but to do it...
 		mainDrive.arcadeDrive(lin, rot);
@@ -462,6 +470,63 @@ public class Robot extends TimedRobot {
 
 		// Keep trying...
 		return false;
+	}
+
+	/*
+	 * Auto Turn
+	 * 
+	 * Call from auto state machine, when it is finished it will return True so that
+	 * you can go to the next step.
+	 * 
+	 * This function will use the gyro to rotate to a set heading
+	 * 
+	 * Must reset encoders and autoTimer between steps.
+	 */
+	private boolean autoTurn(double heading) {
+
+		// Max drive speed
+		// TODO: Increase / remove after validation.
+		double maxSpeed = 0.4;
+
+		// Update the target heading for autoDrive function
+		dHeading = heading;
+
+		//
+		// Rotation
+		//
+		/** Where is kP_rot derived from? & why 0.5? */
+		double kP_rot = maxSpeed / 45.0; // start slowing down at 45 deg.
+		double e_rot = heading - gyro.getYaw();
+		double rot = e_rot * kP_rot;
+
+		// Max rotation speed
+		rot = absMax(rot, maxSpeed);
+
+		// Nothing left but to do it...
+		mainDrive.arcadeDrive(0.0, rot);
+
+		// Determine if the robot made it to the target
+		// and then wait a bit so that it can correct any overshoot.
+		if (e_rot > 2.0)
+			autoTimer.reset();
+
+		else if (autoTimer.get() > 0.75)
+			return true;
+
+		// Keep trying...
+		return false;
+	}
+
+	// Limits the output of a PWM signal
+	double absMax(double input, double maxValue) {
+
+		// Just in case the max is negative
+		maxValue = Math.abs(maxValue);
+
+		if (input > 0)
+			return Math.min(input, maxValue);
+		else
+			return Math.max(input, -maxValue);
 	}
 
 	private void autoNextStep() {
